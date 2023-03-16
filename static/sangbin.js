336a7f3999e91f28aeda60eccfa7ab5c0e5d48c4
VinylStage <mcpe9869@gmail.com>
2023-03-16 17:48:03 +0900

    js위치 변경, 스크롤 함수재설정

--- static/sangbin.js
+++ static/sangbin.js
@@ -10,9 +10,10 @@ let mainImg = document.querySelector("#vinyl_profile");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  console.log("scrollY", value);
 
-  if (value > 500) {
+  if (value >= 2455) {
+    mainImg.style.animation = "img_disappear 1.3s ease-out forwards";
+  } else if (value <= 1200) {
     mainImg.style.animation = "img_disappear 1.3s ease-out forwards";
   } else {
     mainImg.style.animation = "img_appear 1.3s ease-out";
@@ -23,9 +24,12 @@ let mainText = document.querySelector(".maintext");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  console.log("scrollY", value);
 
-  if (value > 500) {
+  console.log(value);
+
+  if (value >= 2455) {
+    mainText.style.animation = "vin_disappear 1.3s ease-out forwards";
+  } else if (value <= 1200) {
     mainText.style.animation = "vin_disappear 1.3s ease-out forwards";
   } else {
     mainText.style.animation = "vin_appear 1.3s ease-out";
